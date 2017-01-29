538722cb140c24f8bdf319ca3c62ca11cd4f3376
Rudi Visser <rudi@cedita.com>
2017-01-29 11:10:39 +0000

    Further JSON Provider Tests, Package Creation

--- Cedita.Payroll.Tests/JsonProviderTests.cs
+++ Cedita.Payroll.Tests/JsonProviderTests.cs
@@ -3,6 +3,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Cedita.Payroll.Engines;
 using System.Linq;
+using Cedita.Payroll.Models.TaxYearSpecifics;
 
 namespace Cedita.Payroll.Tests
 {
@@ -12,14 +13,40 @@ namespace Cedita.Payroll.Tests
         private IProvideTaxYearSpecifics _provider = new JsonTaxYearSpecificProvider();
 
         [TestCategory("JSON Provider Tests"), TestMethod]
-        public void JsonBracketProvision()
+        public void DefaultJsonProviderTest()
         {
             var provider = new JsonTaxYearSpecificProvider();
             provider.SetTaxYear(2015);
 
             var brackets = provider.GetTaxBrackets();
-
             Assert.AreEqual(31785, brackets.First().To);
+            Assert.AreEqual(150000, brackets.Last().From);
+
+            var niCodeA = provider.GetCodeSpecifics('A');
+            Assert.AreEqual(12, niCodeA.EeD);
+            Assert.AreEqual(13.8m, niCodeA.ErF);
+
+            var fixedCode = provider.GetFixedCode("NT");
+            Assert.AreEqual(0, fixedCode.Rate);
+
+            Assert.AreEqual("1060L", provider.GetSpecificValue<string>(TaxYearSpecificValues.DefaultTaxCode));
+            Assert.AreEqual(40040, provider.GetSpecificValue<decimal>(TaxYearSpecificValues.UpperAccrualPoint));
+            Assert.AreEqual(0.6m, provider.GetSpecificValue<decimal>(TaxYearSpecificValues.DeaProtectedEarnings));
+
+            // Check that we can change tax years
+            provider.SetTaxYear(2017);
+            brackets = provider.GetTaxBrackets();
+            Assert.AreNotEqual(31785, brackets.First().To);
+
+            niCodeA = provider.GetCodeSpecifics('A');
+            Assert.AreEqual(2, niCodeA.EeD);
+            Assert.AreEqual(0, niCodeA.ErF);
+
+            fixedCode = provider.GetFixedCode("BR");
+            Assert.AreEqual(0.2m, fixedCode.Rate);
+
+            Assert.AreEqual("1150L", provider.GetSpecificValue<string>(TaxYearSpecificValues.DefaultTaxCode));
+            Assert.AreEqual(8164, provider.GetSpecificValue<decimal>(TaxYearSpecificValues.PrimaryThreshold));
         }
     }
 }
